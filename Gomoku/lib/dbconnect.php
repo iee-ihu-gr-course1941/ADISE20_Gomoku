1ae00d1a7e00a2faf8d0058abd74780da1999a07
Anna-ar <57010343+Anna-ar@users.noreply.github.com>
2021-01-07 16:18:21 +0200

    update
    Needed for the link, permission granted by Teacher

--- Gomoku/lib/dbconnect.php
+++ Gomoku/lib/dbconnect.php
@@ -10,9 +10,16 @@ $pass=$DB_PASS;
 
 global $mysqli;
 
-$mysqli = new mysqli($host, $user, $pass, $db);    
-if ($mysqli->connect_errno){ 
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2016/it164787/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+
+
 ?>
